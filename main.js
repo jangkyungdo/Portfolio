a77e618cf60c8cb9b9e420212c1ff1ba0818548f
jangkyungdo <60591095+jangkyungdo@users.noreply.github.com>
2020-05-21 15:02:44 +0900

    JS : Navbar

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ const navbarHeight = navbar.getBoundingClientRect().height;
 
 // scroll이라는 이벤트가 발생하면 해당 함수를 호출
 document.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-  console.log(`navbarHeight: ${navbarHeight}`);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
   } else {
