58e11d840f2e670c560fec3e5804b32ec2d09697
jangkyungdo <60591095+jangkyungdo@users.noreply.github.com>
2020-05-21 18:14:34 +0900

    JS : Arrow Up

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 "use strict";
 
+// 1. scroll이라는 이벤트가 발생하면 해당 함수를 호출
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-const home = document.querySelector(".home__container");
-const homeHeight = home.getBoundingClientRect().height;
 
-document.addEventListener("scroll", () => {
-  home.style.opacity = 1 - window.scrollY / homeHeight;
-});
-
-// 1. scroll이라는 이벤트가 발생하면 해당 함수를 호출
 document.addEventListener("scroll", () => {
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("navbar--dark");
   } else {
     navbar.classList.remove("navbar--dark");
   }
+});
+
+// 1-1. home 화면이 내려갈수록 투명하게
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
 // 2. 메뉴 클릭 할 경우 해당위치로 scroll
@@ -42,3 +44,18 @@ function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
 }
+
+// 3. arrowup--btn이 아래로 scroll 될 때 보이도록
+const arrowBtn = document.querySelector(".arrowup--btn");
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight) {
+    arrowBtn.classList.add("visible");
+  } else {
+    arrowBtn.classList.remove("visible");
+  }
+});
+// 3. arrowup--btn을 클릭 할 경우 위로 scroll
+arrowBtn.addEventListener("click", () => {
+  scrollIntoView("#home");
+});
